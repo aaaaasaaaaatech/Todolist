01ea22ef63b4116053b708a26a1cf5ba4f56313b
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-08-17 18:28:26 +0900

    commit22

--- src/main/java/com/example/todolist/common/Utils.java
+++ src/main/java/com/example/todolist/common/Utils.java
@@ -1,5 +1,20 @@
 package com.example.todolist.common;
 
-public class Utils {
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+public class Utils {
+// Todolist4 で追加
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+	public static Date str2date(String s) {
+		long ms = 0;
+		try {
+			ms = sdf.parse(s).getTime();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return new Date(ms);
+	}
 }
