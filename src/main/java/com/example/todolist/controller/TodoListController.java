d195ee168fc15f54f58c5a70d21f988e9166ab88
aaaaasaaaaatech <132049862+aaaaasaaaaatech@users.noreply.github.com>
2023-08-17 09:51:30 +0900

    controller

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -2,16 +2,20 @@ package com.example.todolist.controller;
 
 import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.todolist.entity.Todo;
 import com.example.todolist.form.TodoData;
 import com.example.todolist.repository.TodoRepository;
 import com.example.todolist.service.TodoService;
+
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -19,6 +23,8 @@ import lombok.AllArgsConstructor;
 public class TodoListController {
 	private final TodoRepository todoRepository;
 	private final TodoService todoService; // Todolist2 で追加
+	private final HttpSession session;
+
 // ToDo 一覧表示(Todolist で追加)
 
 	@GetMapping("/todo")
@@ -35,26 +41,26 @@ public class TodoListController {
 	public ModelAndView createTodo(ModelAndView mv) {
 		mv.setViewName("todoForm"); // ①
 		mv.addObject("todoData", new TodoData()); // ②
+		session.setAttribute("mode", "create"); // ③
 		return mv;
 	}
 
 // ToDo 追加処理(Todolist2 で追加)
 // 【処理 2 】 ToDo 入力画面(todoForm.html)で[登録]ボタンがクリックされたとき
+	// ToDo 追加処理(Todolist2 で追加したものを Todolist3 で改善)
 	@PostMapping("/todo/create")
-	public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData, // ③
-			BindingResult result, ModelAndView mv) {
+	public String createTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, Model model) {
 		// エラーチェック
-		boolean isValid = todoService.isValid(todoData, result); // ④
+		boolean isValid = todoService.isValid(todoData, result);
 		if (!result.hasErrors() && isValid) {
 			// エラーなし
-			Todo todo = todoData.toEntity(); // ⑤
+			Todo todo = todoData.toEntity();
 			todoRepository.saveAndFlush(todo);
-			return showTodoList(mv);
+			return "redirect:/todo";
 		} else {
 			// エラーあり
-			mv.setViewName("todoForm"); // ⑥
-			// mv.addObject("todoData", todoData);
-			return mv;
+			// model.addAttribute("todoData", todoData);
+			return "todoForm";
 		}
 	}
 
@@ -64,4 +70,36 @@ public class TodoListController {
 	public String cancel() {
 		return "redirect:/todo";
 	}
+
+	@GetMapping("/todo/{id}")
+	public ModelAndView todoById(@PathVariable(name = "id") int id, ModelAndView mv) {
+		mv.setViewName("todoForm");
+		Todo todo = todoRepository.findById(id).get(); // ①
+		mv.addObject("todoData", todo); // ※ b
+		session.setAttribute("mode", "update"); // ②
+		return mv;
+	}
+
+	@PostMapping("/todo/update")
+	public String updateTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, Model model) {
+		// エラーチェック
+		boolean isValid = todoService.isValid(todoData, result);
+		if (!result.hasErrors() && isValid) {
+			// エラーなし
+			Todo todo = todoData.toEntity();
+			todoRepository.saveAndFlush(todo); // ①
+			return "redirect:/todo";
+		} else {
+			// エラーあり
+			// model.addAttribute("todoData", todoData);
+			return "todoForm";
+		}
+	}
+
+	@PostMapping("/todo/delete")
+	public String deleteTodo(@ModelAttribute TodoData todoData) {
+		todoRepository.deleteById(todoData.getId());
+		return "redirect:/todo";
+	}
+
 }
